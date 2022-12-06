e58ce9db5ef672d3ec1417297b09a1b41dc6efa4
proto <proto@LAPTOP-TND8NS36.education.lakanal.epf.fr>
2022-12-06 01:16:28 +0100

    reprise fenetre de jeu jusqu'à mi-vidéo 3

---
+++ sp4_Graphique_Morand_Proton/src/sp4_console/CelluleGraphique.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console;
+import javax.swing.JButton;
+/**
+ *
+ * @author proto
+ */
+public class CelluleGraphique extends JButton{
+    CelluleDeGrille celluleAssociee;
+    for (int i = 5; i>=0 ;i--){
+    for (int j=5;j<7;j++){
+    }
+    }
+}
+ 
